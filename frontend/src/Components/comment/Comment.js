4f9e3f146565ae632af9ccdba90d744ed3b7884b
David Sommers <davidcsommers@gmail.com>
2020-05-11 22:29:43 -0700

    Implement minor transition when comment deleted 1) Tried to use react-flip-toolkit, but didn't work cleanly. See comment in Comments.js file. Ended up just applying setTimeout on a comment to have it fadeout for a sec before deleting it

--- frontend/src/Components/comment/Comment.js
+++ frontend/src/Components/comment/Comment.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import { useDispatch } from 'react-redux';
 import { deleteCommentFromApi } from '../../redux/actions';
 import ListGroup from 'react-bootstrap/ListGroup';
+import './Comment.css';
 
-function Comment({ comment, postId }) {
+function Comment({ comment, postId, ...restOfProps }) {
+  const [isExiting, setIsExiting] = useState(false);
+  const timerId = useRef(null);
   const dispatch = useDispatch();
   const { id, text } = comment;
 
+  useEffect(() => {
+    if (isExiting) {
+      timerId.current = setTimeout(() => (
+        dispatch(deleteCommentFromApi(postId, id))
+      ), 500);
+    }
+    return () => clearTimeout(timerId.current);
+  }, [isExiting]);
+
   return (
-    <ListGroup.Item className="p-2">
-        <i onClick={() => dispatch(deleteCommentFromApi(postId, id))} className="align-bottom text-warning fas fa-times"></i>
+    <ListGroup.Item className={`Comment p-2 ${isExiting ? "fadeOut" : ''}`} {...restOfProps} >
+        <i onClick={() => setIsExiting(true)} className="align-bottom text-warning fas fa-times"></i>
         <span className="ml-3">{text}</span>
     </ListGroup.Item>
   )
--- frontend/src/Components/comment/Comments.js
+++ frontend/src/Components/comment/Comments.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import Comment from './Comment';
 import CommentForm from './CommentForm';
-import { Row, Col, ListGroup } from 'react-bootstrap'
+import { Row, Col, ListGroup } from 'react-bootstrap';
 
 function Comments({ postId, comments }) {
-  // const comments = useSelector(st => st.posts[postId].comments);
 
   const commentsListJSX = comments.map(
-    comment => <Comment key={comment.id} comment={comment} postId={postId}/>
+    comment => (
+        <Comment key={comment.id} comment={comment} postId={postId}/>
+    )
   );
 
   return (
-    <div>
+    <div flipKey={comments.length} className="Comments">
       <h2 className="mb-3">Comments</h2>
       <Row>
         <Col xs={12} lg={10} xl={9}>
@@ -26,3 +27,20 @@ function Comments({ postId, comments }) {
 }
 
 export default Comments;
+
+/*
+  Tried using react-flip-toolkit to cleanly animate component exiting then
+other list items rearranging around it.
+  Worked pretty well sometimes- when lower list items would move up. But
+sometimes upper list items would come down, and it was janky. Part of the jank
+was that the non-list elements on the page were not part of the flipper
+transition, so they would see an empty layout space and transition down/up 
+right away. Would feel weird to put the whole page in a Flipper...
+  Other issue was that when adding comments, once you hit the point of the end
+  of the page where you start to scroll, they randomly try to animate a reordering...
+  Ended up just setting a timer for fadeout on the deleted element. And no 
+  transition for replacing it- they just immediately takeover its spot when it
+  dissapears.
+
+  I thought this link would help: https://github.com/aholachek/react-flip-toolkit/issues/20. But not entirely.
+*/
